f415e8465bf8fe621fa0dd2a6ee688f6b9be2194
zazabe <michel@zazabe.fr>
2010-09-26 15:23:55 +0700

    improve skill view +

--- apps/front/modules/skill/templates/_list.php
+++ apps/front/modules/skill/templates/_list.php
@@ -12,14 +12,10 @@ foreach($skillPager as $skill)
 		echo _open('ul.skills');
 		foreach($descendants as $descendant)
 		{
-			echo _tag('li.level'.$descendant->experience, _tag('span.spacer', str_repeat('> ', $descendant->level-1)._link($descendant)).
+			echo _tag('li.level'.$descendant->experience, _tag('span.spacer', str_repeat('&nbsp;&nbsp;', $descendant->level-1)._tag('span',$descendant)).
 							_tag('div.skillbar.off', 
 								_tag('div.skillbar.on')));
-		/*	
-			echo _tag('li.level'.$descendant->experience, _tag('span.spacer', str_repeat('> ', $descendant->level-1)._link($descendant)).
-							_tag('div.skillbar.off  style="background-position:'.($descendant->experience*25.6).'px 0px;"', 
-							_tag('div.skillbar.on style="width:'.($descendant->experience*25.6).'px;"')));
-		*/
+		
 		}
 		echo _close('ul');
 		echo _close('div');		
